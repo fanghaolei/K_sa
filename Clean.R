1961f9204401136a2f360bb0f60535fce851837a
Michael(Haolei) Fang <fanghaolei@gmail.com>
2016-03-29 17:40:06 -0500

    updated features

--- Clean.R
+++ Clean.R
@@ -1,7 +1,9 @@
 library(data.table)
 library(lubridate)
+train <- data.table(train)
+test <- data.table(test)
 
-# a function count number of words describing colors
+# a function to count number of words describing colors
 word_count <- function(x){
     cleaned <- gsub("/", " ", x)
     parsed  <- strsplit(cleaned, " ")
@@ -9,38 +11,42 @@ word_count <- function(x){
 return(count)    
 }
 
+# create a name frequency table
+train[, INI := substr(Name, 1, 1)]
+train[INI == "3", Name := "Buster"]
+train[INI == "'", Name := "Stache"]
+train[INI == " ", Name := c("Joanie", "Mario")]
+train[, INI := substr(Name, 1, 1)]
+test[, INI := substr(Name, 1, 1)]
+names <- c(train$Name, test$Name)
+name_freq <- table(names)/length(names)
+
+# create a color frequency table
+colors <- c(train$Color, test$Color)
+colors <- gsub("/", " ", colors)
+split_col <- unlist(strsplit(colors, " "))
+col_freq <- table(split_col)/length(split_col)
+
+
 # a clean function for training and testing
 clean <- function(df){
     dt <- data.table(df)
-    # parse names
-    cat("parsing names...\n")
-    dt[, INI := substr(Name, 1, 1)]
-    dt[INI == "3", Name := "Buster"]
-    dt[INI == "'", Name := "Stache"]
-    dt[INI == " ", Name := c("Joanie", "Mario")]
-    dt[, INI := substr(Name, 1, 1)]
-    
-    # name is common or not? by frequency?
-    cat("sorting nanes into frequency groups...\n")
+     # name is common or not? by frequency?
+    cat("calculating name frequency...\n")
     n_freq <- vector("numeric", length = nrow(dt))
-    n_table <- table(dt[, Name])
-    for(i in seq_along(dt[, Name])){
-        n_freq[i] <- n_table[dt[i, Name]]
+    for(i in 1:nrow(dt)){
+        n_freq[i] <- name_freq[dt[i, Name]]
     }
-    dt[, name_f := .(n_freq/nrow(dt))]
-    dt[is.na(name_f), name_f := -1] #xgboost can handle this
-    # create a new feature named 1:Yes, 0:No
-    cat("simplifying names...\n")
-    dt[INI == "", named := 0]
-    dt[INI != "", named := 1]
-    
+    dt[, name_f := .(n_freq)]
+    dt[is.na(name_f), name_f := .(name_freq[1])] 
+
     # parse date
     cat("parsing date...\n")
     dt[, DateTime:= ymd_hms(DateTime)]
     dt[, time := .(hour(DateTime) + minute(DateTime)/60)]
-    dt[, year := factor(lubridate::year(DateTime), ordered = T)]
-    dt[, month := factor(lubridate::month(DateTime), ordered = T)]
-    dt[, day := factor(lubridate::day(DateTime), ordered =T)]
+    dt[, year := as.numeric(lubridate::year(DateTime), ordered = T)]
+    dt[, month := as.numeric(lubridate::month(DateTime), ordered = T)]
+    dt[, day := as.numeric(lubridate::day(DateTime), ordered =T)]
     dt[, weekday := lubridate::wday(DateTime, label = T)]
     
     # parse sex and status
@@ -50,8 +56,8 @@ clean <- function(df){
     dt[SexuponOutcome %like% "Female", sex := "Female"]
     
     dt[, status := "Unknown"]
-    dt[SexuponOutcome %like% "Neutered", status := "Sterilized"]
-    dt[SexuponOutcome %like% "Spayed", status := "Sterilized"]
+    dt[SexuponOutcome %like% "Neutered", status := "Neutered"]
+    dt[SexuponOutcome %like% "Spayed", status := "Spayed"]
     dt[SexuponOutcome %like% "Intact", status := "Intact"]
     
     # form age in days
@@ -70,34 +76,30 @@ clean <- function(df){
     dt[Breed %like% "Mix", Breed := "Mix"]
     dt[!Breed %like% "Mix", Breed := "Pure"]
     
-    # simplify color
-    cat("simplifying colors into numbers...\n")
-    color_count <- vector("numeric", length = nrow(dt))
-    for(i in seq_along(dt[, Color])){
-        color_count[i] <- word_count(dt[i, Color])
-    }
-    dt[, color := .(color_count)]
-    
     # further categorizing color.....
-    
-    
-    
-    
+    cat("calculating color frequencies...\n")
+    color_f <- vector("numeric", length = nrow(dt))
+    dt[, Color := gsub("/", " ", Color)]
+    for(i in 1:nrow(dt)){
+        color_p <- unlist(strsplit(dt[i, Color], " "))
+        color_f[i] <- sum(col_freq[color_p])
+    }
+    dt[, color_freq := .(color_f)]
     
     # drop orginal features
     cat("dropping orginal features...\n")
     if("OutcomeType" %in% colnames(df)){
         print("training set")
-        dt <- dt[, .(OutcomeType, AnimalType, Breed, named, name_f, year, month, 
-                     day, weekday, age, time, sex, status, color)]
-        to_factors <- c("OutcomeType", "AnimalType", "Breed", 
-                        "named",  "sex", "status")
+        dt <- dt[, .(OutcomeType, AnimalType, Breed, name_f, year, month, 
+                     day, weekday, age, time, sex, status, color_freq)]
+        to_factors <- c("OutcomeType", "AnimalType", "weekday", "month", "Breed", 
+                         "sex", "status")
     } else {
         print("testing set")
-        dt <- dt[, .(AnimalType, Breed, named, name_f, year, month, day, weekday, 
-                     age, time, sex, status, color)]
-        to_factors <- c("AnimalType", "Breed", 
-                        "named",  "sex", "status")
+        dt <- dt[, .(AnimalType, Breed, name_f, year, month, day, weekday, 
+                     age, time, sex, status, color_freq)]
+        to_factors <- c("AnimalType", "month", "weekday", "Breed", 
+                        "sex", "status")
     }
     
     # convert to factor
@@ -106,11 +108,15 @@ clean <- function(df){
         set(dt, j = col, value = as.factor(dt[[col]]))
     }
     
-    for_imp <- which(is.na(dt), arr.ind =T)
+    for_imp <- which(is.na(dt), arr.ind = T)
     cat("these missing values need to be imputed: \n")
     print(for_imp)
 return(dt)
 }
+
+
+
+
 
 dt.train <- clean(train)
 dt.test <- clean(test)
